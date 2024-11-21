9d4f008dd3a439c298f4c6e599e6f0807ce0e003
Shahbaz Ansari <shahbaz4783@gmail.com>
2024-11-22 01:33:38 +0530

    fix reserved entities

--- api/markdown/reply-start.ts
+++ api/markdown/reply-start.ts
@@ -2,7 +2,7 @@ export const replyStartCaption = (name: string) => {
 	return `
 Hey *${name}\\!* Welcome to Powerplay Arena\\!
 
-*Dive into a world of cricket action and thrilling bets.
-*Two games, endless excitement!
+*Dive into a world of cricket action and thrilling bets\\.
+*Two games\\, endless excitement\\!
   `;
 };
